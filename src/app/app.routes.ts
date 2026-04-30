70a207bec37ce8171403b671b3b0a698a62c93c3
Obregon <jesusarmando854@gmail.com>
2026-04-29 20:55:20 -0700

    Corregir imágenes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Servicios } from './pages/servicios/servicios';
+import { Equipo } from './pages/equipo/equipo';
+import { Contacto } from './pages/contacto/contacto';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'servicios', component: Servicios },
+  { path: 'equipo', component: Equipo },
+  { path: 'contacto', component: Contacto },
+  { path: '**', component: NotFound }
+];
